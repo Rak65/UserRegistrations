3817dba5a79464bf86ec016ebfd163548440bf37
Rak65 <rakeshp6505@gmail.com>
2023-11-15 16:55:07 +0530

    [Added] : Refactor the code using Lambda expressions and functional interface.

--- main/java/com/userregistration/UserRegistration.java
+++ main/java/com/userregistration/UserRegistration.java
@@ -3,50 +3,27 @@ package com.userregistration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@FunctionalInterface
+interface ValidationRule{
+    boolean test(String input);
+}
 public class UserRegistration {
     private static final String FIRST_NAME_PATTERN = "^[A-Z][a-zA-Z]{2,}$";
     private static final String LAST_NAME_PATTERN = "^[A-Z][a-zA-Z]{2,}$";
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,4}$";
-    public boolean validateFirstName(String firstName){
-        if (firstName != null){
-            Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
-            Matcher matcher = pattern.matcher(firstName);
-            return matcher.matches();
-        }
-        return false;
-    }
-    public boolean validateLastName(String lastName){
-        if (lastName != null){
-            Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
-            Matcher matcher = pattern.matcher(lastName);
-            return matcher.matches();
-        }
-        return false;
-    }
-    public boolean validateEmail(String email){
-        if(email!=null){
-            Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-            Matcher matcher = pattern.matcher(email);
-            return matcher.matches();
-        }
-        return false;
-    }
 
-    public boolean validateMobileNumber(String mobileNumber) {
-        if (mobileNumber != null && mobileNumber.matches("^91\\s\\d{10}$")){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    public boolean validatePassword(String password){
-        return password != null &&
-                password.length() >= 8 &&
-                password.matches(".*[A-Z].*") &&
-                password.matches(".*\\d.*") &&
-                password.matches(".*[!@#$%^&*()].*") &&
-                !password.matches(".*[!@#$%^&*()].*[!@#$%^&*()].*");
-    }
+    public ValidationRule isValidFirstName= firstName -> firstName != null && Pattern.compile(FIRST_NAME_PATTERN).matcher(firstName).matches();
 
+    public ValidationRule isValidLastName= lastName -> lastName != null && Pattern.compile(FIRST_NAME_PATTERN).matcher(lastName).matches();
+
+    public ValidationRule isValidEmail = email -> email != null && Pattern.compile(EMAIL_PATTERN).matcher(email).matches();
+
+    public ValidationRule isValidMobileNumber = mobileNumber -> mobileNumber != null && mobileNumber.matches("^91\\s\\d{10}$");
+    public ValidationRule isValidPassword = password ->
+                    password != null &&
+                    password.length() >= 8 &&
+                    password.matches(".*[A-Z].*") &&
+                    password.matches(".*\\d.*") &&
+                    password.matches(".*[!@#$%^&*()].*") &&
+                    !password.matches(".*[!@#$%^&*()].*[!@#$%^&*()].*");
 }
--- test/java/com/userregistration/UserRegistrationTest.java
+++ test/java/com/userregistration/UserRegistrationTest.java
@@ -9,86 +9,67 @@ public class UserRegistrationTest {
     @Test
     public void testValidFirstName(){
         UserRegistration userRegistration= new UserRegistration();
-        Assertions.assertTrue(userRegistration.validateFirstName("Rakesh"));
-        Assertions.assertTrue(userRegistration.validateFirstName("Sumesh"));
+        Assertions.assertTrue(userRegistration.isValidFirstName.test ("Rakesh"));
+        Assertions.assertTrue(userRegistration.isValidFirstName.test("Sumesh"));
     }
     @Test
     public void testInvalidFirstName(){
         UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertFalse(userRegistration.validateFirstName("R"));
-        Assertions.assertFalse(userRegistration.validateFirstName("Ra"));
-        Assertions.assertFalse(userRegistration.validateFirstName("rakesh"));
-        Assertions.assertFalse(userRegistration.validateFirstName("1234"));
+        Assertions.assertFalse(userRegistration.isValidFirstName.test("R"));
+        Assertions.assertFalse(userRegistration.isValidFirstName.test("Ra"));
+        Assertions.assertFalse(userRegistration.isValidFirstName.test("rakesh"));
+        Assertions.assertFalse(userRegistration.isValidFirstName.test("1234"));
     }
     @Test
     public void testValidLastName(){
         UserRegistration userRegistration= new UserRegistration();
-        Assertions.assertTrue(userRegistration.validateLastName("Rakesh"));
-        Assertions.assertTrue(userRegistration.validateLastName("Sumesh"));
+        Assertions.assertTrue(userRegistration.isValidLastName.test("Rakesh"));
+        Assertions.assertTrue(userRegistration.isValidLastName.test("Sumesh"));
     }
     @Test
     public void testInvalidLastName(){
         UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertFalse(userRegistration.validateLastName("R"));
-        Assertions.assertFalse(userRegistration.validateLastName("Ra"));
-        Assertions.assertFalse(userRegistration.validateLastName("rakesh"));
-        Assertions.assertFalse(userRegistration.validateLastName("1234"));
+        Assertions.assertFalse(userRegistration.isValidLastName.test("R"));
+        Assertions.assertFalse(userRegistration.isValidLastName.test("Ra"));
+        Assertions.assertFalse(userRegistration.isValidLastName.test("rakesh"));
+        Assertions.assertFalse(userRegistration.isValidLastName.test("1234"));
     }
     @Test
     public void testValidEmail(){
         UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertTrue(userRegistration.validateEmail("abc@yahoo.com"));
-        Assertions.assertTrue(userRegistration.validateEmail("abc-100@yahoo.com"));
-        Assertions.assertTrue(userRegistration.validateEmail("abc.100@yahoo.com"));
-        Assertions.assertTrue(userRegistration.validateEmail("abc111@abc.com"));
-        Assertions.assertTrue(userRegistration.validateEmail("abc-100@abc.net"));
-        Assertions.assertTrue(userRegistration.validateEmail("abc.100@abc.com.au"));
-        Assertions.assertTrue(userRegistration.validateEmail("abc@1.com"));
-        Assertions.assertTrue(userRegistration.validateEmail("abc.100@gmail.com.com"));
-        Assertions.assertTrue(userRegistration.validateEmail("abc+100@gmail.com"));
+        Assertions.assertTrue(userRegistration.isValidEmail.test("abc@yahoo.com"));
+        Assertions.assertTrue(userRegistration.isValidEmail.test("abc-100@yahoo.com"));
     }
     @Test
     public void testInValidEmail() {
         UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertFalse(userRegistration.validateEmail("abc@bl"));
-        Assertions.assertFalse(userRegistration.validateEmail("abc.xyz@bl@co.in"));
-        Assertions.assertFalse(userRegistration.validateEmail("abc"));
-        Assertions.assertFalse(userRegistration.validateEmail("abc.gmail.com"));
-        Assertions.assertFalse(userRegistration.validateEmail("abc.gmail.com.1a"));
-        Assertions.assertFalse(userRegistration.validateEmail("abc.gmail.com.aa.au"));
+        Assertions.assertFalse(userRegistration.isValidEmail.test("abc@bl"));
+        Assertions.assertFalse(userRegistration.isValidEmail.test("abc.xyz@bl@co.in"));
     }
-    @Test
-    public void testValidMobileNumber(){
+    @ParameterizedTest
+    @CsvSource({
+            "91 7988984543, true",
+            "91 7988984543, true",
+            "91 7968984543, true",
+            "94633978, false",
+            "91 7988947543, true"
+    })
+    public void testValidMobileNumber(String mobileNo, boolean expected){
         UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertTrue(userRegistration.validateMobileNumber("91 7988984543"));
-        Assertions.assertTrue(userRegistration.validateMobileNumber("91 7958984543"));
-        Assertions.assertTrue(userRegistration.validateMobileNumber("91 7968984543"));
-        Assertions.assertTrue(userRegistration.validateMobileNumber("91 7984684543"));
-        Assertions.assertTrue(userRegistration.validateMobileNumber("91 7988947543"));
+        boolean result = userRegistration.isValidMobileNumber.test(mobileNo);
+        Assertions.assertEquals(expected,result);
     }
-    @Test
-    public void testInValidMobileNumber(){
+    @ParameterizedTest
+    @CsvSource({
+            "PassDord1!, true",
+            "P@ssw1rW, true",
+            "NoDigits@, false",
+            "NoSpecials123, false"
+    })
+    public void testValidPassword(String password, boolean expected){
         UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertFalse(userRegistration.validateEmail("94633978"));
-        Assertions.assertFalse(userRegistration.validateEmail("94633978"));
-        Assertions.assertFalse(userRegistration.validateEmail("91 94633978"));
-        Assertions.assertFalse(userRegistration.validateEmail("913494633978"));
-        Assertions.assertFalse(userRegistration.validateEmail("91 94633978"));
-    }
-    @Test
-    public void testValidPassword(){
-        UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertTrue(userRegistration.validatePassword("PassDord1!"));
-        Assertions.assertTrue(userRegistration.validatePassword("P@ssw1rW"));
-    }
-    @Test
-    public void testInvalidPassword(){
-        UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertFalse(userRegistration.validatePassword("short"));
-        Assertions.assertFalse(userRegistration.validatePassword("NoDigits@"));
-        Assertions.assertFalse(userRegistration.validatePassword("nouppercase1!"));
-        Assertions.assertFalse(userRegistration.validatePassword("toomany!!!specials"));
-        Assertions.assertFalse(userRegistration.validatePassword("NoSpecials123"));
+        boolean result = userRegistration.isValidPassword.test(password);
+        Assertions.assertEquals(expected,result);
     }
     @ParameterizedTest
     @CsvSource({
@@ -101,7 +82,7 @@ public class UserRegistrationTest {
     })
     public void testEmailValidation(String email, boolean expected) {
         UserRegistration userRegistration = new UserRegistration();
-        boolean isValid = userRegistration.validateEmail(email);
+        boolean isValid = userRegistration.isValidEmail.test(email);
         Assertions.assertEquals(expected, isValid);
     }
 }
